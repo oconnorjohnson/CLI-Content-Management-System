e72e0ce78309c8fab9465e57e20726557079bb35
thaflavorsaver <dojohnson@ucdavis.edu>
2022-07-03 15:25:50 -0700

    added view employee by manager const

--- server.js
+++ server.js
@@ -247,3 +247,114 @@ const addNewRole = () => {
       })
     });
   }
+
+  const updateRole = () => {
+    connection.query("SELECT * FROM EMPLOYEE", (err, emplRes) => {
+      if (err) throw err;
+      const employeeChoice = [];
+      emplRes.forEach(({ first_name, last_name, id }) => {
+        employeeChoice.push({
+          name: first_name + " " + last_name,
+          value: id
+        });
+      });
+      
+      connection.query("SELECT * FROM ROLE", (err, rolRes) => {
+        if (err) throw err;
+        const roleChoice = [];
+        rolRes.forEach(({ title, id }) => {
+          roleChoice.push({
+            name: title,
+            value: id
+            });
+          });
+       
+        let questions = [
+          {
+            type: "list",
+            name: "id",
+            choices: employeeChoice,
+            message: "whose role do you want to update?"
+          },
+          {
+            type: "list",
+            name: "role_id",
+            choices: roleChoice,
+            message: "what is the employee's new role?"
+          }
+        ]
+    
+        inquirer.prompt(questions)
+          .then(response => {
+            const query = `UPDATE EMPLOYEE SET ? WHERE ?? = ?;`;
+            connection.query(query, [
+              {role_id: response.role_id},
+              "id",
+              response.id
+            ], (err, res) => {
+              if (err) throw err;
+              
+              console.log("successfully updated employee's role!");
+              startPrompt();
+            });
+          })
+          .catch(err => {
+            console.error(err);
+          });
+        })
+    });
+  }
+
+  const viewEmployeeByManager =  () => {
+    connection.query("SELECT * FROM EMPLOYEE", (err, emplRes) => {
+      if (err) throw err;
+      const employeeChoice = [{
+        name: 'None',
+        value: 0
+      }];
+      emplRes.forEach(({ first_name, last_name, id }) => {
+        employeeChoice.push({
+          name: first_name + " " + last_name,
+          value: id
+        });
+      });
+       
+      let questions = [
+        {
+          type: "list",
+          name: "manager_id",
+          choices: employeeChoice,
+           message: "whose role do you want to update?"
+        },
+      ]
+    
+      inquirer.prompt(questions)
+        .then(response => {
+          let manager_id, query;
+          if (response.manager_id) {
+            query = `SELECT E.id AS id, E.first_name AS first_name, E.last_name AS last_name, 
+            R.title AS role, D.name AS department, CONCAT(M.first_name, " ", M.last_name) AS manager
+            FROM EMPLOYEE AS E LEFT JOIN ROLE AS R ON E.role_id = R.id
+            LEFT JOIN DEPARTMENT AS D ON R.department_id = D.id
+            LEFT JOIN EMPLOYEE AS M ON E.manager_id = M.id
+            WHERE E.manager_id = ?;`;
+          } else {
+            manager_id = null;
+            query = `SELECT E.id AS id, E.first_name AS first_name, E.last_name AS last_name, 
+            R.title AS role, D.name AS department, CONCAT(M.first_name, " ", M.last_name) AS manager
+            FROM EMPLOYEE AS E LEFT JOIN ROLE AS R ON E.role_id = R.id
+            LEFT JOIN DEPARTMENT AS D ON R.department_id = D.id
+            LEFT JOIN EMPLOYEE AS M ON E.manager_id = M.id
+            WHERE E.manager_id is null;`;
+          }
+          connection.query(query, [response.manager_id], (err, res) => {
+            if (err) throw err;
+            console.table(res);
+            startPrompt();
+          });
+        })
+        .catch(err => {
+          console.error(err);
+        }); 
+    });
+  }
