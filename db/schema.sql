292b216a719883759fd7dcecbf464d9e01d10728
thaflavorsaver <dojohnson@ucdavis.edu>
2022-07-02 17:29:45 -0700

    started schema.sql.

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS employee_DB; 
+CREATE database employee_DB; 
+
+USE employee_DB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+)
