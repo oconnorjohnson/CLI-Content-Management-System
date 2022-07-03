fd46a6cfb4bc9c362bcc69c3aa513aba1f111f3e
thaflavorsaver <dojohnson@ucdavis.edu>
2022-07-02 17:37:36 -0700

    finished schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -18,3 +18,18 @@ CREATE TABLE role (
         ON DELETE CASCADE ON UPDATE CASCADE 
 );
 
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMEMNT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL, 
+    last_name VARCHAR(30) NOT NULL, 
+    role_id INT NOT NULL, 
+    manager_id INT NULL, 
+    FOREIGN KEY (role_id)
+        REFERENCES role(id)
+        ON DELETE RESTRICT ON UPDATE CASCADE, 
+    FOREIGN KEY (manager_id)
+        REFERENCES employee(id)
+        ON DELETE SET NULL 
+        ON UPDATE CASCADE 
+);
+
