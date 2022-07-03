f882b5c10850c8635d9e7b7922cbe8123248eb88
thaflavorsaver <dojohnson@ucdavis.edu>
2022-07-03 15:47:21 -0700

    added delete department const

--- server.js
+++ server.js
@@ -248,26 +248,26 @@ const addNewRole = () => {
     });
   }
 
-  const updateRole = () => {
+const updateRole = () => {
     connection.query("SELECT * FROM EMPLOYEE", (err, emplRes) => {
-      if (err) throw err;
-      const employeeChoice = [];
-      emplRes.forEach(({ first_name, last_name, id }) => {
-        employeeChoice.push({
-          name: first_name + " " + last_name,
-          value: id
+        if (err) throw err;
+        const employeeChoice = [];
+        emplRes.forEach(({ first_name, last_name, id }) => {
+            employeeChoice.push({
+                name: first_name + " " + last_name,
+                value: id
+            });
         });
-      });
       
-      connection.query("SELECT * FROM ROLE", (err, rolRes) => {
+    connection.query("SELECT * FROM ROLE", (err, rolRes) => {
         if (err) throw err;
         const roleChoice = [];
         rolRes.forEach(({ title, id }) => {
           roleChoice.push({
-            name: title,
-            value: id
+                name: title,
+                value: id
             });
-          });
+        });
        
         let questions = [
           {
@@ -285,134 +285,172 @@ const addNewRole = () => {
         ]
     
         inquirer.prompt(questions)
-          .then(response => {
-            const query = `UPDATE EMPLOYEE SET ? WHERE ?? = ?;`;
-            connection.query(query, [
-              {role_id: response.role_id},
-              "id",
-              response.id
-            ], (err, res) => {
-              if (err) throw err;
+            .then(response => {
+                const query = `UPDATE EMPLOYEE SET ? WHERE ?? = ?;`;
+                connection.query(query, [
+                    {role_id: response.role_id},
+                    "id",
+                    response.id
+                ], (err, res) => {
+                    if (err) throw err;
               
               console.log("successfully updated employee's role!");
               startPrompt();
-            });
-          })
+                });
+            })
           .catch(err => {
             console.error(err);
-          });
+            });
         })
     });
-  }
+}
 
-  const viewEmployeeByManager =  () => {
+const viewEmployeeByManager =  () => {
     connection.query("SELECT * FROM EMPLOYEE", (err, emplRes) => {
-      if (err) throw err;
-      const employeeChoice = [{
-        name: 'None',
-        value: 0
-      }];
-      emplRes.forEach(({ first_name, last_name, id }) => {
-        employeeChoice.push({
-          name: first_name + " " + last_name,
-          value: id
+        if (err) throw err;
+        const employeeChoice = [{
+            name: 'None',
+            value: 0
+        }];
+        emplRes.forEach(({ first_name, last_name, id }) => {
+            employeeChoice.push({
+                name: first_name + " " + last_name,
+                value: id
+            });
         });
-      });
        
-      let questions = [
-        {
-          type: "list",
-          name: "manager_id",
-          choices: employeeChoice,
-           message: "whose role do you want to update?"
-        },
-      ]
+        let questions = [
+            {
+                type: "list",
+                name: "manager_id",
+                choices: employeeChoice,
+                message: "whose role do you want to update?"
+            },
+        ]
     
-      inquirer.prompt(questions)
+    inquirer.prompt(questions)
         .then(response => {
-          let manager_id, query;
-          if (response.manager_id) {
-            query = `SELECT E.id AS id, E.first_name AS first_name, E.last_name AS last_name, 
-            R.title AS role, D.name AS department, CONCAT(M.first_name, " ", M.last_name) AS manager
-            FROM EMPLOYEE AS E LEFT JOIN ROLE AS R ON E.role_id = R.id
-            LEFT JOIN DEPARTMENT AS D ON R.department_id = D.id
-            LEFT JOIN EMPLOYEE AS M ON E.manager_id = M.id
-            WHERE E.manager_id = ?;`;
-          } else {
-            manager_id = null;
-            query = `SELECT E.id AS id, E.first_name AS first_name, E.last_name AS last_name, 
-            R.title AS role, D.name AS department, CONCAT(M.first_name, " ", M.last_name) AS manager
-            FROM EMPLOYEE AS E LEFT JOIN ROLE AS R ON E.role_id = R.id
-            LEFT JOIN DEPARTMENT AS D ON R.department_id = D.id
-            LEFT JOIN EMPLOYEE AS M ON E.manager_id = M.id
-            WHERE E.manager_id is null;`;
-          }
-          connection.query(query, [response.manager_id], (err, res) => {
-            if (err) throw err;
-            console.table(res);
-            startPrompt();
-          });
+            let manager_id, query;
+            if (response.manager_id) {
+                query = `SELECT E.id AS id, E.first_name AS first_name, E.last_name AS last_name, 
+                R.title AS role, D.name AS department, CONCAT(M.first_name, " ", M.last_name) AS manager
+                FROM EMPLOYEE AS E LEFT JOIN ROLE AS R ON E.role_id = R.id
+                LEFT JOIN DEPARTMENT AS D ON R.department_id = D.id
+                LEFT JOIN EMPLOYEE AS M ON E.manager_id = M.id
+                WHERE E.manager_id = ?;`;
+        } else {
+                manager_id = null;
+                query = `SELECT E.id AS id, E.first_name AS first_name, E.last_name AS last_name, 
+                R.title AS role, D.name AS department, CONCAT(M.first_name, " ", M.last_name) AS manager
+                FROM EMPLOYEE AS E LEFT JOIN ROLE AS R ON E.role_id = R.id
+                LEFT JOIN DEPARTMENT AS D ON R.department_id = D.id
+                LEFT JOIN EMPLOYEE AS M ON E.manager_id = M.id
+                WHERE E.manager_id is null;`;
+            }
+            connection.query(query, [response.manager_id], (err, res) => {
+                if (err) throw err;
+                console.table(res);
+                startPrompt();
+            });
         })
         .catch(err => {
-          console.error(err);
+            console.error(err);
         }); 
     });
   }
 
-  const updateManager = ()=> {
+const updateManager = ()=> {
     connection.query("SELECT * FROM EMPLOYEE", (err, emplRes) => {
-      if (err) throw err;
-      const employeeChoice = [];
-      emplRes.forEach(({ first_name, last_name, id }) => {
-        employeeChoice.push({
-          name: first_name + " " + last_name,
-          value: id
+        if (err) throw err;
+        const employeeChoice = [];
+        emplRes.forEach(({ first_name, last_name, id }) => {
+            employeeChoice.push({
+                name: first_name + " " + last_name,
+                value: id
+            });
         });
-      });
       
-      const managerChoice = [{
+    const managerChoice = [{
         name: 'None',
         value: 0
-      }]; 
-      emplRes.forEach(({ first_name, last_name, id }) => {
-        managerChoice.push({
-          name: first_name + " " + last_name,
-          value: id
+    }]; 
+        emplRes.forEach(({ first_name, last_name, id }) => {
+            managerChoice.push({
+                name: first_name + " " + last_name,
+                value: id
+            });
         });
-      });
        
-      let questions = [
-        {
-          type: "list",
-          name: "id",
-          choices: employeeChoice,
-          message: "who do you want to update?"
-        },
-        {
-          type: "list",
-          name: "manager_id",
-          choices: managerChoice,
-          message: "whos is the employee's new manager?"
-        }
-      ]
+            let questions = [
+            {
+                type: "list",
+                name: "id",
+                choices: employeeChoice,
+                message: "who do you want to update?"
+            },
+            {
+                type: "list",
+                name: "manager_id",
+                choices: managerChoice,
+                message: "whos is the employee's new manager?"
+            }
+        ]
     
-      inquirer.prompt(questions)
+    inquirer.prompt(questions)
         .then(response => {
-          const query = `UPDATE EMPLOYEE SET ? WHERE id = ?;`;
-          let manager_id = response.manager_id !== 0? response.manager_id: null;
-          connection.query(query, [
-            {manager_id: manager_id},
-            response.id
-          ], (err, res) => {
-            if (err) throw err;
+            const query = `UPDATE EMPLOYEE SET ? WHERE id = ?;`;
+            let manager_id = response.manager_id !== 0? response.manager_id: null;
+            connection.query(query, [
+                {manager_id: manager_id},
+                response.id
+            ], (err, res) => {
+                if (err) throw err;
               
             console.log("successfully updated employee's manager");
             startPrompt();
-          });
+            });
         })
         .catch(err => {
-          console.error(err);
+            console.error(err);
         });
     })
     
+};
+
+const deleteDepartment = () => {
+    const departments = [];
+    connection.query("SELECT * FROM DEPARTMENT", (err, res) => {
+        if (err) throw err;
+  
+        res.forEach(dep => {
+            let qObj = {
+            name: dep.name,
+            value: dep.id
+            }
+            departments.push(qObj);
+        });
+  
+    let questions = [
+        {
+            type: "list",
+            name: "id",
+            choices: departments,
+            message: "which department do u want to delete?"
+        }
+    ];
+  
+        inquirer.prompt(questions)
+        .then(response => {
+            const query = `DELETE FROM DEPARTMENT WHERE id = ?`;
+            connection.query(query, [response.id], (err, res) => {
+                if (err) throw err;
+                console.log(`${res.affectedRows} row(s) successfully deleted!`);
+            startPrompt();
+            });
+        })
+      .catch(err => {
+        console.error(err);
+      });
+    });
   };
+  
