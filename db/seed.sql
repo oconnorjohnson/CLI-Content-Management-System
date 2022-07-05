1acb61c082a2d27042a6cd492389dd7341f0833b
Daniel O'Connor Johnson <100829940+oconnorjohnson@users.noreply.github.com>
2022-07-05 14:44:25 -0700

    Update seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -1,28 +1,28 @@
 INSERT INTO department (name)
-VALUES ("engineering"),
-    ("finance"),
-    ("marketing"),
-    ("sales");
+VALUES ("development"),
+    ("accounting"),
+    ("IT"),
+    ("management");
 
 SELECT * FROM DEPARTMENT; 
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("software engineer", 120000, 1), 
-    ("project manager", 90000, 1), 
-    ("enegineering manager", 225000, 1), 
-    ("accountant", 70000, 2), 
-    ("accounting manager", 120000, 2), 
-    ("product marketing manager", 50000, 3),
-    ("marketing lead", 150000, 3),
-    ("sales rep", 85000, 4); 
+VALUES ("senior software engineer", 500000, 1), 
+    ("accountant", 120000, 2), 
+    ("software engineer L1", 150000, 1), 
+    ("IT Specialist", 100000, 3), 
+    ("Head Accountant", 250000, 2), 
+    ("CEO", 1000000, 4),
+    ("Upper Management 2", 450000, 4),
+    ("Upper Management 3", 300000, 4); 
 
 SELECT * FROM ROLE; 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Spike", "Bulldog", 3, NULL),
-    ("Tom", "Cat", 3, 1),
-    ("Jerry", "Mouse", 1, 2),
-    ("Mickey", "Mouse", 1, 2),
-    ("Toots", "Mouse", 2, 1);
+VALUES ("Cameron", "Eskrine", 2, 4),
+    ("Cooper", "Johnson", 4, NULL),
+    ("Colin", "French", 1, 4),
+    ("Daniel", "Johnson", 1, 4),
+    ("Jada", "Macias", 3, 4);
 
 SELECT * FROM employee;
